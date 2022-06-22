253fdabb4b186598bb33f7be2f1c85d521320457
Akash52 <ac8572611@gmail.com>
2022-06-22 10:26:06 +0530

    initial setup

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1 class="text-4xl">Hello World</h1>
+    <p>This is a simple example of a Vue.js component.</p>
+    {{ couter }}
+  </div>
+</template>
+<script>
+import { mapState } from "vuex";
+export default {
+  computed: {
+    ...mapState({
+      counter: (state) => state.counter,
+    }),
+  },
+};
+</script>
