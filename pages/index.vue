6b63d7c1ae3e0f6bd5920f2f98748fefd1e9ee13
Akash52 <ac8572611@gmail.com>
2022-08-01 22:06:05 +0530

    update-vuex-state-with-mutations-and-mapmutations-in-vuejs

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,32 @@
 <template>
-  <div>
-    <h1 class="text-4xl">Hello World</h1>
-    <p>This is a simple example of a Vue.js component.</p>
-    {{ couter }}
+  <div class="flex justify-center mt-8">
+    <button
+      class="text-white text-lg font-bold bg-blue-700 px-4 py-2 rounded"
+      @click="increment"
+    >
+      +
+    </button>
+    <h1 class="text-4xl mr-2 ml-2">
+      {{ counter }}
+    </h1>
+    <button
+      class="text-white text-lg font-bold bg-blue-700 px-4 py-2 rounded"
+      @click="decrement"
+    >
+      -
+    </button>
   </div>
 </template>
 <script>
-import { mapState } from "vuex";
+import { mapState, mapMutations } from "vuex";
 export default {
   computed: {
     ...mapState({
       counter: (state) => state.counter,
     }),
+  },
+  methods: {
+    ...mapMutations(["increment", "decrement"]),
   },
 };
 </script>
